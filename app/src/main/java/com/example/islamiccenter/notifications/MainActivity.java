69caaed02b3053f4c93777193e3c31bae4a29c27
hesham <heshamhamdi77889@gmail.com>
2017-10-18 02:20:23 +0200

    intialz

--- app/src/main/java/com/example/islamiccenter/notifications/MainActivity.java
+++ app/src/main/java/com/example/islamiccenter/notifications/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.islamiccenter.notifications;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.onesignal.OneSignal;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        OneSignal.idsAvailable(new OneSignal.IdsAvailableHandler() {
+            @Override
+            public void idsAvailable(String userId, String registrationId) {
+                Log.d("hesham", "idsAvailable: "+userId);
+            }
+        });
     }
 }
